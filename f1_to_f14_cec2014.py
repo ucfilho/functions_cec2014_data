089f02e5fd9dbe96e8714f3e824534ab39b7c6fb
ucfilho <42356381+ucfilho@users.noreply.github.com>
2022-03-10 14:28:35 -0300

    Update f1_to_f14_cec2014.py

--- f1_to_f14_cec2014.py
+++ f1_to_f14_cec2014.py
@@ -99,3 +99,11 @@ def BiasValue():
               'F8':800.0, 'F9':900.0,'F10':1000.0, 'F11':1100.0,'F12':1200.0, 'F13':1300.0,'F14':1400.0}
     return result
 
+def NamesFunctions():
+    result ={ 'F1': 'elliptic','F2': 'bent_cigar','F3':'discus','F4':'Rosenbrock',
+             'F5': 'Ackey','F6':'Weierstrass','F7':'Griewank','F8':'Rastrigin',
+             'F9':'modified_Schwefel','F10':'Katsuura','F11':'happy_cat',
+             'F12':'hgbat','F13':'expanded_Griewank','F14':'expanded_Scaffer'}
+    return result
+# F13: expanded Griewank   F14:  expanded Scaffer
+
