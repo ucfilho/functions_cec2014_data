2c1bc6477414296fc3708f34bbb15520dfca3da0
ucfilho <42356381+ucfilho@users.noreply.github.com>
2022-03-10 16:16:13 -0300

    Create cec2014_names.py

---
+++ cec2014_names.py
@@ -0,0 +1,13 @@
+
+def BiasValue():
+    result = {'F1':100.0, 'F2':200.0,'F3':300.0, 'F4':400.0,'F5':500.0, 'F6':600.0,'F7':700.0, 'F8':800.0,
+              'F8':800.0, 'F9':900.0,'F10':1000.0, 'F11':1100.0,'F12':1200.0, 'F13':1300.0,'F14':1400.0}
+    return result
+
+def NamesFunctions():
+    result ={ 'F1': 'elliptic','F2': 'bent_cigar','F3':'discus','F4':'Rosenbrock',
+             'F5': 'Ackey','F6':'Weierstrass','F7':'Griewank','F8':'Rastrigin',
+             'F9':'modified_Schwefel','F10':'Katsuura','F11':'happy_cat',
+             'F12':'hgbat','F13':'expanded_Griewank','F14':'expanded_Scaffer'}
+    return result
+# F13: expanded Griewank   F14:  expanded Scaffer
