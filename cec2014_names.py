71b922032722d70032b44532292956aa2d0f2533
ucfilho <42356381+ucfilho@users.noreply.github.com>
2022-03-11 11:57:02 -0300

    Update cec2014_names.py

--- cec2014_names.py
+++ cec2014_names.py
@@ -1,13 +1,26 @@
 
 def BiasValue():
     result = {'F1':100.0, 'F2':200.0,'F3':300.0, 'F4':400.0,'F5':500.0, 'F6':600.0,'F7':700.0, 'F8':800.0,
-              'F8':800.0, 'F9':900.0,'F10':1000.0, 'F11':1100.0,'F12':1200.0, 'F13':1300.0,'F14':1400.0}
+              'F8':800.0, 'F9':900.0,'F10':1000.0, 'F11':1100.0,'F12':1200.0, 'F13':1300.0,'F14':1400.0,
+              'F15':1500.0, 'F16':1600.0,'F17':1700.0, 'F18':1800.0,'F19':1900.0, 'F20':2000.0,'F21':2100.0,
+              'F22':2200.0, 'F23':2300.0,'F24':2400.0, 'F25':2500.0,'F26':2600.0, 'F27':2700.0,'F28':2800.0,
+              'F29':2900.0, 'F30':3000.0}
     return result
 
 def NamesFunctions():
-    result ={ 'F1': 'elliptic','F2': 'bent_cigar','F3':'discus','F4':'Rosenbrock',
-             'F5': 'Ackey','F6':'Weierstrass','F7':'Griewank','F8':'Rastrigin',
-             'F9':'modified_Schwefel','F10':'Katsuura','F11':'happy_cat',
-             'F12':'hgbat','F13':'expanded_Griewank','F14':'expanded_Scaffer'}
+    result = {'F1':"Rotated High Conditioned Elliptic Function", 'F2':"Rotated Bent Cigar Function",
+              'F3':"Rotated Discus Function", 
+              'F4':"Shifted and Rotated Rosenbrock’s Function",'F5':"Rotated Discus Function", 
+              'F6':"Shifted and Rotated Weierstrass Function",'F7':"Shifted and Rotated Griewank’s Function", 
+              'F8':"Shifted Rastrigin’s Function", 'F9':"Shifted and Rotated Rastrigin’s Function",
+              'F10':"Shifted Schwefel’s Function", 'F11':"Shifted and Rotated Schwefel’s Function",
+              'F12':"Shifted and Rotated Katsuura Function", 'F13':"Shifted and Rotated HappyCat Function",
+              'F14':"Shifted and Rotated HGBat Function",'F15':"Shifted and Rotated Expanded Griewank’s plus Rosenbrock’s Function", 
+              'F16':"Shifted and Rotated Expanded Scaffer’s F6 Function",'F17':"Hybrid Function 1", 
+              'F18':"Hybrid Function 2",'F19':"Hybrid Function 3", 
+              'F20':"Hybrid Function 4",'F21':"Hybrid Function 5",
+              'F22':"Hybrid Function 6", 'F23':"Composition Function 1",'F24':"Composition Function 2", 
+              'F25':"Composition Function 3",'F26':"Composition Function 4", 'F27':"Composition Function 5",
+              'F28':"Composition Function 6",'F29':"Composition Function 7", 'F30':"Composition Function 8"}
     return result
 # F13: expanded Griewank   F14:  expanded Scaffer
