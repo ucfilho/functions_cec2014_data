785a7b562d349e27571f7ea6b6e32877a14c3523
ucfilho <42356381+ucfilho@users.noreply.github.com>
2022-03-12 16:38:40 -0300

    Update cec2014_shift_rotate.py

--- cec2014_shift_rotate.py
+++ cec2014_shift_rotate.py
@@ -258,7 +258,8 @@ def F17(solution, shift_data, matrix,shuffle):
     idx2 = shuffle[n1:(n1+n2)]
     idx3 = shuffle[(n1+n2):]
     mz = dot(solution - shift_data, matrix)
-    result = f9_modified_schwefel__(mz[idx1]) + f8_rastrigin__(mz[idx2]) + f1_elliptic__(mz[idx3])
+    # result = f9_modified_schwefel__(mz[idx1]) + f8_rastrigin__(mz[idx2]) + f1_elliptic__(mz[idx3])
+    result = f8_rastrigin__(mz[idx2]) + f1_elliptic__(mz[idx3])
     
     return result
 
