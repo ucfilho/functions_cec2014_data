b32badc26685453e44b33f6e1bfbf359697259b1
ucfilho <42356381+ucfilho@users.noreply.github.com>
2022-03-20 17:33:50 -0300

    Update F1_to_F27_cec2014.py

--- F1_to_F27_cec2014.py
+++ F1_to_F27_cec2014.py
@@ -591,7 +591,7 @@ def F19_aux()#solution=None, name="Hybrid Function 3", shift_data_file="shift_da
     idx4 = shuffle[n1+n2+n3:]
     mz = dot(solution - shift_data, matrix)
     return f7_griewank__(mz[idx1]) + f6_weierstrass__(mz[idx2]) + f4_rosenbrock__(mz[idx3]) + f14_expanded_scaffer__(mz[idx4])+ bias
-'''
+
 def F29():
 # def F29(solution=None, shift_data=None, matrix=None,f_bias=None):
     num_funcs = 3
@@ -626,7 +626,7 @@ def F29():
     for i in range(0, num_funcs):
         result += (weights[i] / sw) * fits[i]
     return result + f_bias
-'''
+
 def F20_aux()#solution=None, name="Hybrid Function 4",  shift_data, matrix,f_bias, bias=2000, shuffle=None):
     problem_size = len(solution)
     p = array([0.2, 0.2, 0.3, 0.3])
@@ -721,7 +721,7 @@ def F22_aux(solution=None, name="Hybrid Function 6", shift_data_file="shift_data
     mz = dot(solution - shift_data, matrix)
     return f10_katsuura__(mz[idx1]) + f11_happy_cat__(mz[idx2]) + f13_expanded_griewank__(mz[idx3]) + \
            f9_modified_schwefel__(mz[idx4]) + f5_ackley__(mz[idx5]) + 
-'''
+
 def F30(solution=None, shift_data=None, matrix=None,f_bias=None):
     num_funcs = 3
     #   "F30":"Composition Function 8"
