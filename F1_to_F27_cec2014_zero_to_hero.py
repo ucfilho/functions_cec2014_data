6925f8da04b24b1d06901e99ec71a3ae9b14905a
ucfilho <42356381+ucfilho@users.noreply.github.com>
2022-03-21 19:13:22 -0300

    Update F1_to_F27_cec2014_zero_to_hero.py

--- F1_to_F27_cec2014_zero_to_hero.py
+++ F1_to_F27_cec2014_zero_to_hero.py
@@ -192,283 +192,57 @@ def F16(solution):
 
 def F17(solution):
     #   "F17":""Hybrid Function 1"
-  
     return solution
 
-def F18(solution=None, shift_data=None, matrix=None,f_bias=None):
+def F18(solution):
     #   "F18":""Hybrid Function 2"
-    problem_size = len(solution)
-    shift_data = shift_data[:problem_size]
-    p = array([0.3, 0.3, 0.4])
-    n1 = int(ceil(p[0] * problem_size))
-    n2 = int(ceil(p[1] * problem_size))
-
-    shuffle = ( shift_data - ones(problem_size)).astype(int)
-    idx1 = shuffle[:n1]
-    idx2 = shuffle[n1:(n1 + n2)]
-    idx3 = shuffle[(n1 + n2):]
-    mz = dot(solution - shift_data, matrix)
-    return f2_bent_cigar__(mz[idx1]) + f12_hgbat__(mz[idx2]) + f8_rastrigin__(mz[idx3]) #+ bias
+    return solution
 
 
-def F19(solution=None, shift_data=None, matrix=None,f_bias=None):
+def F19(solution):
     #   "F19":""Hybrid Function 3"
-    problem_size = len(solution)
-    shift_data = shift_data[:problem_size]
-    p = array([0.2, 0.2, 0.3, 0.3])
-    n1 = int(ceil(p[0] * problem_size))
-    n2 = int(ceil(p[1] * problem_size))
-    n3 = int(ceil(p[2] * problem_size))
-
-    shuffle = (shift_data  - ones(problem_size)).astype(int)
-    idx1 = shuffle[:n1]
-    idx2 = shuffle[n1:(n1 + n2)]
-    idx3 = shuffle[(n1 + n2):(n1+n2+n3)]
-    idx4 = shuffle[n1+n2+n3:]
-    mz = dot(solution - shift_data, matrix)
-    return f7_griewank__(mz[idx1]) + f6_weierstrass__(mz[idx2]) + f4_rosenbrock__(mz[idx3]) + f14_expanded_scaffer__(mz[idx4]) #+ bias
+    return solution
 
 
-def F20(solution=None, shift_data=None, matrix=None,f_bias=None):
+def F20(solution):
     #   "F20":""Hybrid Function 4"
-    problem_size = len(solution)
-    shift_data = shift_data[:problem_size]
-    p = array([0.2, 0.2, 0.3, 0.3])
-    n1 = int(ceil(p[0] * problem_size))
-    n2 = int(ceil(p[1] * problem_size))
-    n3 = int(ceil(p[2] * problem_size))
+    return solution
 
 
-    shuffle = ( shift_data - ones(problem_size)).astype(int)
-    idx1 = shuffle[:n1]
-    idx2 = shuffle[n1:(n1 + n2)]
-    idx3 = shuffle[(n1 + n2):(n1 + n2 + n3)]
-    idx4 = shuffle[n1 + n2 + n3:]
-    mz = dot(solution - shift_data, matrix)
-    result = f12_hgbat__(mz[idx1]) + f3_discus__(mz[idx2]) + f13_expanded_griewank__(mz[idx3]) + f8_rastrigin__(mz[idx4]) #+ bias
-    return result
-
-
-def F21(solution=None, shift_data=None, matrix=None,bias=None):
+def F21(solution):
     #   "F21":""Hybrid Function 5"
-    problem_size = len(solution)
-    shift_data = shift_data[:problem_size]
-    p = array([0.1, 0.2, 0.2, 0.2, 0.3])
-    n1 = int(ceil(p[0] * problem_size))
-    n2 = int(ceil(p[1] * problem_size))
-    n3 = int(ceil(p[2] * problem_size))
-    n4 = int(ceil(p[3] * problem_size))
-
-
-    shuffle = ( shift_data - ones(problem_size)).astype(int)
-    idx1 = shuffle[:n1]
-    idx2 = shuffle[n1:(n1 + n2)]
-    idx3 = shuffle[(n1 + n2):(n1 + n2 + n3)]
-    idx4 = shuffle[(n1+n2+n3):(n1+n2+n3+n4)]
-    idx5 = shuffle[n1+n2+n3+n4:]
-    mz = dot(solution - shift_data, matrix)
-    result =  f14_expanded_scaffer__(mz[idx1]) + f12_hgbat__(mz[idx2]) + f4_rosenbrock__(mz[idx3])
-    result = result + f9_modified_schwefel__(mz[idx4]) + f1_elliptic__(mz[idx5]) #+ bias
-    return result
+    return solution
            
 
 
-def F22(solution=None, shift_data=None, matrix=None,bias=None):
+def F22(solution):
     #   "F22":""Hybrid Function 6"
-    problem_size = len(solution)
-    shift_data = shift_data[:problem_size]
-    p = array([0.1, 0.2, 0.2, 0.2, 0.3])
-    n1 = int(ceil(p[0] * problem_size))
-    n2 = int(ceil(p[1] * problem_size))
-    n3 = int(ceil(p[2] * problem_size))
-    n4 = int(ceil(p[3] * problem_size))
-
-
-    shuffle = (shift_data - ones(problem_size)).astype(int)
-    idx1 = shuffle[:n1]
-    idx2 = shuffle[n1:(n1 + n2)]
-    idx3 = shuffle[(n1 + n2):(n1 + n2 + n3)]
-    idx4 = shuffle[(n1 + n2 + n3):(n1 + n2 + n3 + n4)]
-    idx5 = shuffle[n1 + n2 + n3 + n4:]
-    mz = dot(solution - shift_data, matrix)
-    return f10_katsuura__(mz[idx1]) + f11_happy_cat__(mz[idx2]) + f13_expanded_griewank__(mz[idx3]) + \
-           f9_modified_schwefel__(mz[idx4]) + f5_ackley__(mz[idx5]) + bias
+    return solution
 
 ### ================== Composition function ========================
 
-def F23(solution=None, shift_data=None, matrix=None,f_bias=None):
+def F23(solution):
     #   "F23":"Composition Function 1"
-    problem_size = len(solution)
-    shift_data = shift_data[:problem_size]
-    shift_data = shift_data[:, :problem_size]
-    xichma = array([10, 20, 30, 40, 50])
-    lamda = array([1, 1e-6, 1e-26, 1e-6, 1e-6])
-    bias = array([0, 100, 200, 300, 400])
-
-    # 1. Rotated Rosenbrock’s Function F4’
-    t1 = solution - shift_data[0]
-    g1 = lamda[0] * f4_rosenbrock__(dot(t1, matrix[:problem_size, :])) + bias[0]
-    w1 = (1.0 / sqrt(sum(t1 ** 2))) * exp(-sum(t1 ** 2) / (2 * problem_size * xichma[0] ** 2))
-
-    # 2. High Conditioned Elliptic Function F1’
-    t2 = solution - shift_data[1]
-    g2 = lamda[1] * f1_elliptic__(solution) + bias[1]
-    w2 = (1.0 / sqrt(sum(t2 ** 2))) * exp(-sum(t2 ** 2) / (2 * problem_size * xichma[1] ** 2))
-
-    # 3. Rotated Bent Cigar Function F2’
-    t3 = solution - shift_data[2]
-    g3 = lamda[2] * f2_bent_cigar__(dot(matrix[2 * problem_size: 3 * problem_size, :], t3)) + bias[2]
-    w3 = (1.0 / sqrt(sum(t3 ** 2))) * exp(-sum(t3 ** 2) / (2 * problem_size * xichma[2] ** 2))
-
-    # 4. Rotated Discus Function F3’
-    t4 = solution - shift_data[3]
-    g4 = lamda[3] * f3_discus__(dot(matrix[3 * problem_size: 4 * problem_size, :], t4)) + bias[3]
-    w4 = (1.0 / sqrt(sum(t4 ** 2))) * exp(-sum(t4 ** 2) / (2 * problem_size * xichma[3] ** 2))
-
-    # 4. High Conditioned Elliptic Function F1’
-    t5 = solution - shift_data[4]
-    g5 = lamda[4] * f1_elliptic__(solution) + bias[4]
-    w5 = (1.0 / sqrt(sum(t5 ** 2))) * exp(-sum(t5 ** 2) / (2 * problem_size * xichma[4] ** 2))
-
-    sw = sum([w1, w2, w3, w4, w5])
-    result = (w1 * g1 + w2 * g2 + w3 * g3 + w4 * g4 + w5 * g5) / sw
-    return result #+ f_bias
+    return solution
 
 
-def F24(solution=None, shift_data=None, matrix=None,f_bias=None):
+def F24(solution):
     #   "F24":"Composition Function 2"
-    problem_size = len(solution)
-    shift_data = shift_data[:problem_size]
-    shift_data = shift_data[:, :problem_size]
-    xichma = array([20, 20, 20])
-    lamda = array([1, 1, 1])
-    bias = array([0, 100, 200])
-
-    # 1. Rotated Rosenbrock’s Function F4’
-    t1 = solution - shift_data[0]
-    g1 = lamda[0] * f9_modified_schwefel__(solution) + bias[0]
-    w1 = (1.0 / sqrt(sum(t1 ** 2))) * exp(-sum(t1 ** 2) / (2 * problem_size * xichma[0] ** 2))
-
-    # 2. Rotated Rastrigin’s Function F9’
-    t2 = solution - shift_data[1]
-    g2 = lamda[1] * f8_rastrigin__(dot(matrix[problem_size: 2 * problem_size], t2)) + bias[1]
-    w2 = (1.0 / sqrt(sum(t2 ** 2))) * exp(-sum(t2 ** 2) / (2 * problem_size * xichma[1] ** 2))
-
-    # 3. Rotated HGBat Function F14’
-    t3 = solution - shift_data[2]
-    g3 = lamda[2] * f12_hgbat__(dot(matrix[2 * problem_size: 3 * problem_size, :], t3)) + bias[2]
-    w3 = (1.0 / sqrt(sum(t3 ** 2))) * exp(-sum(t3 ** 2) / (2 * problem_size * xichma[2] ** 2))
-
-    sw = sum([w1, w2, w3])
-    result = (w1 * g1 + w2 * g2 + w3 * g3) / sw
-    return result #+ f_bias
+    return solution
 
 
-def F25(solution=None, shift_data=None, matrix=None,f_bias=None):
+def F25(solution):
     #   "F25":"Composition Function 3"
-    problem_size = len(solution)
-    shift_data = shift_data[:problem_size]
-    shift_data = shift_data[:, :problem_size]
-    xichma = array([10, 30, 50])
-    lamda = array([0.25, 1, 1e-7])
-    bias = array([0, 100, 200])
-
-    # 1. Rotated Schwefel's Function F11’
-    t1 = solution - shift_data[0]
-    g1 = lamda[0] * f9_modified_schwefel__(dot(matrix[:problem_size, :problem_size], t1)) + bias[0]
-    w1 = (1.0 / sqrt(sum(t1 ** 2))) * exp(-sum(t1 ** 2) / (2 * problem_size * xichma[0] ** 2))
-
-    # 2. Rotated Rastrigin’s Function F9’
-    t2 = solution - shift_data[1]
-    g2 = lamda[1] * f8_rastrigin__(dot(matrix[problem_size: 2 * problem_size], t2)) + bias[1]
-    w2 = (1.0 / sqrt(sum(t2 ** 2))) * exp(-sum(t2 ** 2) / (2 * problem_size * xichma[1] ** 2))
-
-    # 3. Rotated High Conditioned Elliptic Function F1'
-    t3 = solution - shift_data[2]
-    g3 = lamda[2] * f1_elliptic__(dot(matrix[2 * problem_size: 3 * problem_size, :], t3)) + bias[2]
-    w3 = (1.0 / sqrt(sum(t3 ** 2))) * exp(-sum(t3 ** 2) / (2 * problem_size * xichma[2] ** 2))
-
-    sw = sum([w1, w2, w3])
-    result = (w1 * g1 + w2 * g2 + w3 * g3) / sw
-    return result #+ f_bias
+    return solution
 
 
-def F26(solution=None, shift_data=None, matrix=None,f_bias=None):
-    #   "F26":"Composition Function 4"
-    problem_size = len(solution)
-    shift_data = shift_data[:problem_size]
-    shift_data = shift_data[:, :problem_size]
-    xichma = array([10, 10, 10, 10, 10])
-    lamda = array([0.25, 1, 1e-7, 2.5, 10])
-    bias = array([0, 100, 200, 300, 400])
-
-    # 1. Rotated Schwefel's Function F11’
-    t1 = solution - shift_data[0]
-    g1 = lamda[0] * f9_modified_schwefel__(dot(matrix[:problem_size, :], t1)) + bias[0]
-    w1 = (1.0 / sqrt(sum(t1 ** 2))) * exp(-sum(t1 ** 2) / (2 * problem_size * xichma[0] ** 2))
-
-    # 2. Rotated HappyCat Function F13’
-    t2 = solution - shift_data[1]
-    g2 = lamda[1] * f11_happy_cat__(dot(matrix[problem_size:2 * problem_size, :], t2)) + bias[1]
-    w2 = (1.0 / sqrt(sum(t2 ** 2))) * exp(-sum(t2 ** 2) / (2 * problem_size * xichma[1] ** 2))
-
-    # 3. Rotated High Conditioned Elliptic Function F1’
-    t3 = solution - shift_data[2]
-    g3 = lamda[2] * f1_elliptic__(dot(matrix[2 * problem_size: 3 * problem_size, :], t3)) + bias[2]
-    w3 = (1.0 / sqrt(sum(t3 ** 2))) * exp(-sum(t3 ** 2) / (2 * problem_size * xichma[2] ** 2))
-
-    # 4. Rotated Weierstrass Function F6’
-    t4 = solution - shift_data[3]
-    g4 = lamda[3] * f6_weierstrass__(dot(matrix[3 * problem_size: 4 * problem_size, :], t4)) + bias[3]
-    w4 = (1.0 / sqrt(sum(t4 ** 2))) * exp(-sum(t4 ** 2) / (2 * problem_size * xichma[3] ** 2))
-
-    # 5. Rotated Griewank’s Function F7’
-    t5 = solution - shift_data[4]
-    g5 = lamda[4] * f7_griewank__(dot(matrix[4*problem_size:, :], t5)) + bias[4]
-    w5 = (1.0 / sqrt(sum(t5 ** 2))) * exp(-sum(t5 ** 2) / (2 * problem_size * xichma[4] ** 2))
-
-    sw = sum([w1, w2, w3, w4, w5])
-    result = (w1 * g1 + w2 * g2 + w3 * g3 + w4 * g4 + w5 * g5) / sw
-    return result #+ f_bias
+def F26(solution):
+    return solution
 
 
-def F27(solution=None, shift_data=None, matrix=None,f_bias=None):
+def F27(solution):
     #   "F27":"Composition Function 5"
-    problem_size = len(solution)
-    shift_data = shift_data[:problem_size]
-    shift_data = shift_data[:, :problem_size]
-    xichma = array([10, 10, 10, 20, 20])
-    lamda = array([10, 10, 2.5, 25, 1e-6])
-    bias = array([0, 100, 200, 300, 400])
-
-    # 1. Rotated HGBat Function F14'
-    t1 = solution - shift_data[0]
-    g1 = lamda[0] * f12_hgbat__(dot(matrix[:problem_size, :], t1)) + bias[0]
-    w1 = (1.0 / sqrt(sum(t1 ** 2))) * exp(-sum(t1 ** 2) / (2 * problem_size * xichma[0] ** 2))
-
-    # 2. Rotated Rastrigin’s Function F9’
-    t2 = solution - shift_data[1]
-    g2 = lamda[1] * f8_rastrigin__(dot(matrix[problem_size:2 * problem_size, :], t2)) + bias[1]
-    w2 = (1.0 / sqrt(sum(t2 ** 2))) * exp(-sum(t2 ** 2) / (2 * problem_size * xichma[1] ** 2))
-
-    # 3. Rotated Schwefel's Function F11’
-    t3 = solution - shift_data[2]
-    g3 = lamda[2] * f9_modified_schwefel__(dot(matrix[2 * problem_size: 3 * problem_size, :], t3)) + bias[2]
-    w3 = (1.0 / sqrt(sum(t3 ** 2))) * exp(-sum(t3 ** 2) / (2 * problem_size * xichma[2] ** 2))
-
-    # 4. Rotated Weierstrass Function F6’
-    t4 = solution - shift_data[3]
-    g4 = lamda[3] * f6_weierstrass__(dot(matrix[3 * problem_size: 4 * problem_size, :], t4)) + bias[3]
-    w4 = (1.0 / sqrt(sum(t4 ** 2))) * exp(-sum(t4 ** 2) / (2 * problem_size * xichma[3] ** 2))
-
-    # 5. Rotated High Conditioned Elliptic Function F1’
-    t5 = solution - shift_data[4]
-    g5 = lamda[4] * f1_elliptic__(dot(matrix[4 * problem_size:, :], t5)) + bias[4]
-    w5 = (1.0 / sqrt(sum(t5 ** 2))) * exp(-sum(t5 ** 2) / (2 * problem_size * xichma[4] ** 2))
-
-    sw = sum([w1, w2, w3, w4, w5])
-    result = (w1 * g1 + w2 * g2 + w3 * g3 + w4 * g4 + w5 * g5) / sw
-    return result #+ f_bias
+    return solution
 '''
 def F28():
 # def F28(solution=None, shift_data=None, matrix=None,f_bias=None):
